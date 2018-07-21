a2d594f93977231e2c0ecdcb86fdbebe4145ca93
harish2906 <31368439+harish2906@users.noreply.github.com>
2018-07-21 16:02:45 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
      file {'/root/README':
-          ensure => file,
-          content => 'This is readme file',
+          ensure  => file,
+          content => 'This is readme file \n',
+          owner   => 'root'
       }
 }
